07d491b39605252e294ddfd49b3791d62ece9547
Chitresh Goyal <chitreshstar1@gmail.com>
2018-12-28 13:38:09 +0530

    Reverse & Palindrome

---
+++ reverse_and_palindrome/README.md
@@ -0,0 +1,3 @@
+# Reverse string & Palindrome
+Reverse a string in ruby without using keyword reserve
+Also check if string is palindrome or not
---
+++ reverse_and_palindrome/reverse_string.rb
@@ -0,0 +1,29 @@
+def reverse(string)
+	arr = string.split("")
+	reversed = []
+	arr.size.times { reversed << arr.pop}
+	reversed.join("")
+end
+puts reverse("hello")
+
+def reverse(ary)
+  return [] if ary.empty?
+  reverse(ary.drop(1)) + [ary.first]
+end
+puts reverse("hello".split("")).join("")
+
+def reverse(ary)
+  ary = ary.split("") if ary.is_a?(String)
+  return [] if ary.empty?
+  [ary.last] + reverse(ary[0...-1])
+end
+puts reverse("hello").join("")
+
+
+def palindrome(word)
+  word=word.split("") if word.is_a?(String) 
+  return true if word.empty?
+  word.first == word.last && palindrome(word[1...-1])
+end
+
+puts palindrome("rajadjar")
